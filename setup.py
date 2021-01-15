d5d96ad93b5526af851daa321db86401cc25ed0b
Ajay Kumar <46121016+ajaykumar2409@users.noreply.github.com>
2021-01-15 22:06:53 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='testCases',
+    version='0.1demo',
+    packages=['testCases',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+    url='https://github.com/ajaykumar2409/deep_learning_practise/testCases'
+)
