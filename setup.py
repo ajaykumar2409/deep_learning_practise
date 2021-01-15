80a4f35578bc85b956087755ab1fa1cd9f0cb53a
Ajay Kumar <46121016+ajaykumar2409@users.noreply.github.com>
2021-01-15 23:03:57 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='testCases',
-    packages=['deep_learning_practise',],
-    long_description=open('README.txt').read(),
-    url='https://github.com/ajaykumar2409/deep_learning_practise/testCases'
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ajaykumar2409", # Replace with your own username
+    version="0.0.1",
+    author="Ajay kumar",
+    author_email="ajaylearnml@gmail.com",
+    description="A testCases package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ajaykumar2409/deep_learning_practise",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
